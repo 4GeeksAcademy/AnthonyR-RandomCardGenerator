96ae1131891616e305ef1017134d8035496009e5
Tonyreng <tonyreng01@gmail.com>
2025-04-04 03:29:24 +0000

    Dando estilos a la carta

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let stickArr = ['♦', '♥', '♠', '♣'];
+let sticks = document.querySelectorAll('.stick');
+let cardNumber = document.querySelector('cardNumber');
+
+
+
 window.onload = function() {
   //write your code here
+
+
   console.log("Hello Rigo from the console!");
 };
