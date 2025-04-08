b6fee0c35f1fdd14b996bdf6c379c1972f637f37
Tonyreng <tonyreng01@gmail.com>
2025-04-08 03:14:37 +0000

    Aplicando buenas practicas agregando condicionales para setiar el alto y ancho de la carta y quitando las variables globales

--- src/app.js
+++ src/app.js
@@ -5,50 +5,56 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let stickArr = ['♦', '♥', '♠', '♣'];
-let numbersArr = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 'J', 'Q', 'K'];
-let sticks = document.querySelectorAll('.stick');
-let cardNumber = document.querySelector('.cardNumber');
-let buttonChange = document.getElementById('change');
-let newHeight = document.getElementById('height');
-let newWidth = document.getElementById('width');
-let card = document.querySelector('.card');
-let counter = document.getElementById('counter');
-let time = 5;
-
 const NewCard = () => {
+  let stickArr = ['♦', '♥', '♠', '♣'];
+  let numbersArr = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 'J', 'Q', 'K'];
+  let sticks = document.querySelectorAll('.stick');
+  let cardNumber = document.querySelector('.cardNumber');
   let randomNumber = Math.floor(Math.random() * 12);
   let randomStick = Math.floor(Math.random() * 4);
   let newNumber = numbersArr[randomNumber];
-  let newStick = stickArr[randomStick];
+  let newSticks = stickArr[randomStick];
 
   cardNumber.innerHTML = newNumber;
 
   sticks.forEach(stick => {
-    stick.innerHTML = newStick;
+    stick.innerHTML = newSticks;
 
-    stick.style.color = (newStick === '♥' || newStick === '♦' ? 'red' : 'black');
+    stick.style.color = (newSticks === '♥' || newSticks === '♦' ? 'red' : 'black');
   });
 }
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   NewCard();
 };
 
+let buttonChange = document.getElementById('change');
 buttonChange.addEventListener('click', (e) => {
   NewCard();
 });
 
+
+let card = document.querySelector('.card');
+let newHeight = document.getElementById('height');
 newHeight.addEventListener('change', (e) => {
-  card.style.height = `${e.target.value}px`;
+  if (e.target.value >= 500) {
+    card.style.height = `${e.target.value}px`;
+  }
+
 });
 
+let newWidth = document.getElementById('width');
 newWidth.addEventListener('change', (e) => {
-  card.style.width = `${e.target.value}px`;
+  if (e.target.value >= 350) {
+    card.style.width = `${e.target.value}px`;
+  }
 });
 
+let time = 5;
+let counter = document.getElementById('counter');
 setInterval(() => {
+
   time--;
   counter.textContent = `Cambiando carta en ${time}`;
 
